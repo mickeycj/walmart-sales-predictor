b7ad6d7f6b17b932cf3af3e4bc861ece44e9e56a
Chanon Jenakom <chanonjenakom@gmail.com>
2018-04-03 10:14:41 +0700

    Add comments

--- walmart_knn_regressor.py
+++ walmart_knn_regressor.py
@@ -10,19 +10,23 @@ def transform_and_normalize_date(X):
     for i in range(int(X.size/4)):
         X[i][2] /= max_day
 
+# Retrieve and pre-process the training set.
 train_df = pd.read_csv('train.csv')
 X = train_df[['Store', 'Dept', 'Date', 'IsHoliday']].as_matrix()
 y = train_df[['Weekly_Sales']].as_matrix()
 transform_and_normalize_date(X)
 
+# Train the kNN model.
 knn_regressor = KNeighborsRegressor(n_neighbors=50, weights='distance', p=1)
 knn_regressor.fit(X, y)
 
+# Retrieve and pre-process the testing set.
 test_df = pd.read_csv('test.csv')
 T = test_df[['Store', 'Dept', 'Date', 'IsHoliday']].as_matrix()
 ids = test_df[['Store', 'Dept', 'Date']].as_matrix()
 transform_and_normalize_date(T)
 
+# Write the prediction results to submission.csv file.
 with open('submission.csv', 'w') as file:
     file.write('Id,Weekly_Sales\n')
     for i in range(int(T.size/4)):
